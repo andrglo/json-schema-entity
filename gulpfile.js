c19e9548b744945f93066c3290fec5ef8ebb09c5
Andre Gloria <andre@ayk.com.br>
2015-11-25 11:03:23 -0200

    Dependencies update

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('static', function() {
 });
 
 gulp.task('nsp', function(cb) {
-  nsp({package: __dirname + '/package.json'}, cb);
+  nsp({package: path.join(__dirname, 'package.json')}, cb);
 });
 
 var sources = ['src/index.js', 'src/adapters/postgres.js'];
